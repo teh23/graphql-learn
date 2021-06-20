f59ec8e359f1b9bc346e6022b3a0fdc04e434ee6
23teh <tehix1@gmail.com>
2021-06-20 23:43:56 +0200

    apollo query init

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
+const ALL_AUTHORS = gql`
+    query {
+        allAuthors {
+            name
+        }
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      start
-    </div>
-  );
+    const result = useQuery(ALL_AUTHORS);
+    if (result.loading) {
+        return <div>loading</div>;
+    }
+    return (
+        <div className='App'>
+            {result.data.allAuthors.map((p) => p.name).join(", ")}
+        </div>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
+import {
+    ApolloClient,
+    ApolloProvider,
+    HttpLink,
+    InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: "http://localhost:4000",
+    }),
+});
 
 ReactDOM.render(
-    <React.StrictMode>
+    <ApolloProvider client={client}>
         <App />
-    </React.StrictMode>,
+    </ApolloProvider>,
     document.getElementById("root")
 );
