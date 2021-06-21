ca276841612594fd1fadfe65f5fc203ff4ae2710
23teh <tehix1@gmail.com>
2021-06-21 11:31:31 +0200

    ex 8.11

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { gql, useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
 
 const ALL_AUTHORS = gql`
     query {
@@ -12,8 +12,27 @@ const ALL_AUTHORS = gql`
     }
 `;
 
+const EDIT_AUTHOR = gql`
+    mutation edit($name: String!, $born: Int!) {
+        editAuthor(name: $name, setBornTo: $born) {
+            name
+            born
+        }
+    }
+`;
+
 const Authors = (props) => {
     const authors = useQuery(ALL_AUTHORS, { pollInterval: 3000 });
+    const [editAuthor] = useMutation(EDIT_AUTHOR, {
+        refetchQueries: [{ query: ALL_AUTHORS }],
+    });
+    const [name, setName] = useState("");
+    const [born, setBorn] = useState();
+
+    const changeAuthor = (e) => {
+        e.preventDefault();
+        editAuthor({ variables: { name, born: parseInt(born) } });
+    };
 
     if (!props.show) {
         return null;
@@ -42,6 +61,18 @@ const Authors = (props) => {
                     ))}
                 </tbody>
             </table>
+            <h2>Set birthyear</h2>
+            name
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <br />
+            born
+            <input
+                value={born}
+                input='number'
+                onChange={(e) => setBorn(e.target.value)}
+            />
+            <br />
+            <button onClick={(e) => changeAuthor(e)}>change</button>
         </div>
     );
 };
